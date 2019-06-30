a07b25a20dcfd7f432e2ba7b464e7a421c7ca048
Mikhail <mikhail.khmel@gmail.com>
2019-06-30 15:36:50 +0300

    bugfixes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import telebot
+import os
 import datetime
 import random
 import sqlite3
 import time
 import logging
-import mytoken
+# import mytoken
 
 conn = sqlite3.connect('all_db.sqlite', check_same_thread=False)
 cur = conn.cursor()
@@ -23,7 +24,7 @@ strDelta = str(delta.days)
 
 chat_id_angelina = '399683820'
 chat_id_misha = '216036961'
-token = mytoken.token
+token = '484695046:AAHdIL-gVwIGzbA2-KQAOsi2R5v3P-I-oO8'
 bot = telebot.TeleBot(token)
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
@@ -255,4 +256,25 @@ def handle_text(message):
 #                          "Список свидание пуст :(\nВоспользуйтесь командой 'Создать свидание' на кливиатуре для создания нового свидания")
 
 
-bot.polling(none_stop=True, interval=0)
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        
+        bot.remove_webhook()
+        bot.set_webhook(url="https://ourtelegrambot.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
